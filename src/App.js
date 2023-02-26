77dc4ee661f3591c4e50c63104bbdc59e46883f5
Parsa Parishan <pparsaparishan@yahoo.com>
2023-02-25 21:24:47 -0500

    import data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-
+import { useState } from "react";
+import data from "./data";
 
 function App() {
-  return <div className="App"></div>;
+  const [task, setTask] = useState(data);
+
+  return <div className="App">
+    
+  </div>;
 }
 
 export default App;
