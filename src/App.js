cc888897cce361ea8c69dd6555b6562e4a84b5e3
Parsa Parishan <pparsaparishan@yahoo.com>
2023-02-25 21:40:29 -0500

    import react-bautiful-dnd context

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import { useState } from "react";
 import data from "./data";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 function App() {
   const [tasks, setTasks] = useState(data);
 
   return (
     <div className="App">
-      <main>
-        {tasks.map((task) => {
-          return (
-            <div className="tasks">
-              <h2>{task.title}</h2>
-              {task.tasks.map((section) => {
-                return (
-                  <div className="task">
-                    <h5>{section.title}</h5>
-                  </div>
-                );
-              })}
-            </div>
-          );
-        })}
-      </main>
+      <DragDropContext>
+        <main>
+          {tasks.map((task) => {
+            return (
+              <div className="tasks">
+                <h2>{task.title}</h2>
+                {task.tasks.map((section) => {
+                  return (
+                    <div className="task">
+                      <h5>{section.title}</h5>
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </main>
+      </DragDropContext>
     </div>
   );
 }
