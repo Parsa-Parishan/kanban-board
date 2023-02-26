1454ddfdd77ec4eb2665974392f036b7e1c47e7d
Parsa Parishan <pparsaparishan@yahoo.com>
2023-02-25 21:21:43 -0500

    add mock data

---
+++ src/data.js
@@ -0,0 +1,30 @@
+const data = [
+  {
+    id: `toDo`,
+    title: "To do",
+    tasks: [
+      { id: `Breakfast`, title: "Make Breakfast" },
+      { id: `dog`, title: "Walk the dog" },
+      { id: `English`, title: "English class at 5" },
+    ],
+  },
+  {
+    id: `inProgress`,
+    title: "in progress",
+    tasks: [
+      { id: `friends`, title: "Catch up with friends" },
+      { id: `project`, title: "Personal project" },
+      { id: `parents`, title: "Visit parents" },
+    ],
+  },
+  {
+    id: `Completed`,
+    title: "Completed",
+    tasks: [
+      { id: `appointment`, title: "Make doctor's appointment" },
+      { id: `laundry`, title: "Do laundry" },
+    ],
+  },
+];
+
+export default data;
